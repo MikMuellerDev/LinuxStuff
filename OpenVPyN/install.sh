809b1f18901fb5777a85fb0055f9043e4a63825a
MikMuellerDev <imse334@gmail.com>
2021-05-28 14:27:09 +0200

    Update install.sh

--- OpenVPyN/install.sh
+++ OpenVPyN/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt install python3 python3-pip python-tk python3-tk openvpn
 sudo pip3 install requests
+cd /usr/bin/
+sudo mkdir OpenVPyN
 git clone https://github.com/MikMuellerDev/LinuxStuff
 # shellcheck disable=SC2164
 cd LinuxStuff/
