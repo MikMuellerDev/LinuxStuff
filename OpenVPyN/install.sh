ac31ed2a19ba8f510831fdd933481282fe7fbdd3
MikMuellerDev <imse334@gmail.com>
2021-05-28 12:08:38 +0200

    Update install.sh

--- OpenVPyN/install.sh
+++ OpenVPyN/install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 sudo apt install python3 python3-pip python-tk python3-tk openvpn
-sudo pip3 install requests pyinstaller
+sudo pip3 install requests
 git clone https://github.com/MikMuellerDev/LinuxStuff
 # shellcheck disable=SC2164
-cd LinuxStuff/OpenVPyN/
+cd LinuxStuff/
+sudo rm -R .git
+sudo rm -R .gitignore
+sudo rm -R README.md
+# shellcheck disable=SC2164
+cd OpenVPyN/
 chmod +x main.py
-pyinstaller main.py --onedir
-sudo rm -R __pycache__
-sudo rm -R build
 sudo rm .gitignore
-sudo rm main.spec
 sudo rm README.md
-sudo mv dist/main ./
-sudo rm -R dist
-sudo mv icon.png main/
-sudo mv SECRETSDEMO.py main/
-sudo rm main.py
-sudo rm install.sh
+echo "Installed OpenVPyN."
