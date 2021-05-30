e775678d67d7825528668d9c07ccc4fb1f2c2e9a
MikMuellerDev <imse334@gmail.com>
2021-05-30 22:19:11 +0200

    updated Basic Install

--- BasicInstallScript/install.sh
+++ BasicInstallScript/install.sh
@@ -10,7 +10,6 @@ apt update
 apt upgrade -y
 apt install sudo vim neofetch fish htop curl xclip git openjdk-11-jdk python3.9 python3-pip python3-tk lolcat wget cmatrix fortune cowsay tmux openjfxsnapd speedtest-cli wine figlet tree -y || exit 1
 
-
 pip3 install bpytop openpyxl requests apscheduler --upgrade
 
 # Install bat
@@ -18,21 +17,21 @@ curl -L -o ~/bat_0.18.0_amd64.deb https://github.com/sharkdp/bat/releases/downlo
 dpkg -i ~/bat_0.18.0_amd64.deb
 rm ~/bat_0.18.0_amd64.deb
 
-
-# execute per user on the target system
-getent passwd | while IFS=: read -r name _ uid _ _ home shell; do
+# Execute per user
+getent passwd | while IFS=: read -r name _ uid _ _ home shell; do # name password uid gid gecos home shell
   if [ -d "$home" ] && [ "$(stat -c %u "$home")" = "$uid" ]; then
     if [ -n "$shell" ] && [ "$shell" != "/bin/false" ] && [ "$shell" != "/usr/sbin/nologin" ]; then
-      #  Execute this if the user is not root or uses sudo
+      # If not root
       if [ "$uid" -ne 0 ]; then
         # Install fish
-        #su -c "wget -O- https://raw.githubusercontent.com/RubixDev/random-linux-stuff/main/terminator-plasma-install/install.sh | bash" "$name"
-        echo 'skipped'
+          sudo bash -c "(wget -O- https://raw.githubusercontent.com/MikMuellerDev/LinuxStuff/main/FishConfigurator/install.sh | bash)"
+
+        # Install OpenVPyN
+          wget -O- https://raw.githubusercontent.com/MikMuellerDev/LinuxStuff/main/OpenVPyN/install.sh | bash
       fi
 
       # Install SpaceVim
       su -c "curl -sLf https://spacevim.org/install.sh | bash" "$name"
-
     fi
   fi
 done
